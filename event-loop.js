52d1f3b6657f4280e836f2c9f75381e385ec94ce
Yasin Korkmaz <yasinkorkmaz.dev@gmail.com>
2024-03-10 15:48:22 +0300

    Thread & Event Loop

---
+++ event-loop.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const crypto = require('crypto');
+
+const start = Date.now();
+process.env.UV_THREADPOOL_SIZE = 1;
+
+setTimeout(() => console.log('Timer 1 finished'), 0);
+setImmediate(() => console.log('Immediate 1 finished'));
+
+fs.readFile('test-file.txt', () => {
+  console.log('io finished');
+  console.log('--------------');
+
+  setTimeout(() => console.log('Timer 2 finished'), 0);
+  setTimeout(() => console.log('Timer 3 finished'), 2300);
+  setImmediate(() => console.log('Immediate 2 finished'));
+
+  process.nextTick(() => console.log('process.nexttick'));
+
+  crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () => {
+    console.log(Date.now() - start, 'password encrypted');
+  });
+  crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () => {
+    console.log(Date.now() - start, 'password encrypted');
+  });
+  crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () => {
+    console.log(Date.now() - start, 'password encrypted');
+  });
+  crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () => {
+    console.log(Date.now() - start, 'password encrypted');
+  });
+});
+
+console.log('top level code');
